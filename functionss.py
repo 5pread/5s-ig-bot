8828a91632ec1f4da98901beee46949783bdb438
5pread <68393055+5pread@users.noreply.github.com>
2021-03-19 22:13:17 +0300

    Add files via upload

---
+++ functionss.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import os
+
+path = os.path.abspath(os.getcwd())
+driver_path = r'C:\Users\sbsag\PycharmProjects\Lust.bot\driver\chromedriver.exe'
+
+def Login(usrname, pwd):
+    pass
